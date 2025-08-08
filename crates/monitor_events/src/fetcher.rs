4292a29226ccc7e4ba639d0366513eebfdcd473d
lakewik <wikjezioro@op.pl>
2025-08-08 10:48:07 +0200

    feat: add fetching Optimism outputAtBlock

--- crates/monitor_events/src/fetcher.rs
+++ crates/monitor_events/src/fetcher.rs
@@ -30,6 +30,72 @@ pub struct EvmBlockHeaderFromRpc {
     pub withdrawals_root: Option<String>,
 }
 
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct OptimismOutputAtBlock {
+    pub version: String,
+    pub output_root: String,
+    pub block_ref: BlockRef,
+    pub withdrawal_storage_root: String,
+    pub state_root: String,
+    pub sync_status: SyncStatus,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct BlockRef {
+    pub hash: String,
+    pub number: u64,
+    pub parent_hash: String,
+    pub timestamp: u64,
+    pub l1origin: L1Origin,
+    pub sequence_number: u64,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct L1Origin {
+    pub hash: String,
+    pub number: u64,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct SyncStatus {
+    pub current_l1: L1Block,
+    pub current_l1_finalized: L1Block,
+    pub head_l1: L1Block,
+    pub safe_l1: L1Block,
+    pub finalized_l1: L1Block,
+    pub unsafe_l2: L2Block,
+    pub safe_l2: L2Block,
+    pub finalized_l2: L2Block,
+    pub pending_safe_l2: L2Block,
+    pub cross_unsafe_l2: L2Block,
+    pub local_safe_l2: L2Block,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct L1Block {
+    pub hash: String,
+    pub number: u64,
+    pub parent_hash: String,
+    pub timestamp: u64,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct L2Block {
+    pub hash: String,
+    pub number: u64,
+    pub parent_hash: String,
+    pub timestamp: u64,
+    pub l1origin: L1Origin,
+    pub sequence_number: u64,
+}
+
 pub struct Fetcher {
     pub client: Arc<Client>,
     pub url: String,
@@ -43,7 +109,7 @@ impl Fetcher {
         }
     }
 
-    pub async fn fetch_block_by_number(&self, block_hash: &str) -> Result<EvmBlockHeaderFromRpc> {
+    pub async fn fetch_block_by_hash(&self, block_hash: &str) -> Result<EvmBlockHeaderFromRpc> {
         let rpc_request = json!({
             "jsonrpc": "2.0",
             "method": "eth_getBlockByHash",
@@ -76,5 +142,40 @@ impl Fetcher {
             })?;
 
         Ok(evm_block_header_from_rpc_result)
+    }
+
+    pub async fn fetch_optimism_output_at_block(&self, block_number: &str) -> Result<OptimismOutputAtBlock> {
+        let rpc_request = json!({
+            "jsonrpc": "2.0",
+            "method": "optimism_outputAtBlock",
+            "params": [block_number],
+            "id": 1,
+        });
+
+        let rpc_response = self
+            .client
+            .post(&self.url)
+            .header(header::CONTENT_TYPE, "application/json")
+            .json(&rpc_request)
+            .send()
+            .await?;
+
+        if rpc_response.status() != 200 {
+            return Err(eyre!(
+                "Request failed with status code {}",
+                rpc_response.status()
+            ));
+        }
+
+        let body: Value = rpc_response.json().await?;
+
+        let optimism_output_at_block_from_rpc_result: OptimismOutputAtBlock = body
+            .get("result")
+            .ok_or_eyre("Result field not found")
+            .and_then(|result| {
+                from_value::<OptimismOutputAtBlock>(result.clone()).map_err(Error::msg)
+            })?;
+
+        Ok(optimism_output_at_block_from_rpc_result)
     }
 }
